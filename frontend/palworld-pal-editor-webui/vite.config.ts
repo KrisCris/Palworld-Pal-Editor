3f8d95e6b6c51023879d01317df932e0c9b49599
connlost <hi.imhpc@outlook.com>
2024-02-28 14:43:20 +0800

    add

--- frontend/palworld-pal-editor-webui/vite.config.ts
+++ frontend/palworld-pal-editor-webui/vite.config.ts
@@ -10,11 +10,21 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ registerType: 'autoUpdate', devOptions: {
+      enabled: true
+    }})
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:58080/',
+        changeOrigin: true
+      }
     }
   }
 })
