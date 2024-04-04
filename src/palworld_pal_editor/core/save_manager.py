01375a25f589646034b758ffdc354eee2905bed3
connlost <4348524953.h@gmail.com>
2024-04-04 14:59:40 +0800

    Refine Ghost Pal Detection

--- src/palworld_pal_editor/core/save_manager.py
+++ src/palworld_pal_editor/core/save_manager.py
@@ -221,8 +221,10 @@ class SaveManager:
                     pal_entity = PalEntity(entity)
                     container_id, slot_idx = pal_entity.SlotID
                     group_id = pal_entity.group_id
-                    is_unref_pal = not self.group_data.get_group(group_id).has_pal(pal_entity.InstanceId)
-                    is_unref_pal = is_unref_pal or not self.container_data.get_container(container_id).has_pal(pal_entity.InstanceId, slot_idx)
+                    # is_unref_pal = not self.group_data.get_group(group_id).has_pal(pal_entity.InstanceId)
+                    is_unref_pal = not self.container_data.get_container(container_id).has_pal(pal_entity.InstanceId, slot_idx)
+                    if is_unref_pal:
+                        LOGGER.info(f"Likely Ghost Pal: {pal_entity}")
                     pal_entity.is_unreferenced_pal = is_unref_pal
 
                     owner = pal_entity.OwnerPlayerUId
