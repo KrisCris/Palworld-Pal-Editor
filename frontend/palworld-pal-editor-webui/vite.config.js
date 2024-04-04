f9ae79a36a78869233d03fab770bf52aa112cb3c
connlost <4348524953.h@gmail.com>
2024-04-04 15:39:08 +0800

    remove pwa support since its causing more issue

--- frontend/palworld-pal-editor-webui/vite.config.js
+++ frontend/palworld-pal-editor-webui/vite.config.js
@@ -3,55 +3,55 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    VitePWA({
-      registerType: 'autoUpdate', 
-      workbox: {
-        globPatterns: [],
-        runtimeCaching: [
-          {
-            urlPattern: ({request}) => request.destination === 'image',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images-cache',
-              expiration: {
-                maxEntries: 150,
-                maxAgeSeconds: 24 * 60 * 60,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: ({request}) => request.destination !== 'image',
-            handler: 'NetworkOnly',
-          },
-        ],
-      },
-      devOptions: {
-        enabled: true
-      }, 
-      manifest: {
-        name: 'Palworld Pal Editor',
-        short_name: 'Pal Editor',
-        description: 'Palworld Pal Editor, Authored by _connlost.',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/icons/512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    // VitePWA({
+    //   registerType: 'autoUpdate', 
+    //   workbox: {
+    //     globPatterns: [],
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: ({request}) => request.destination === 'image',
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'images-cache',
+    //           expiration: {
+    //             maxEntries: 150,
+    //             maxAgeSeconds: 24 * 60 * 60,
+    //           },
+    //           cacheableResponse: {
+    //             statuses: [0, 200],
+    //           },
+    //         },
+    //       },
+    //       {
+    //         urlPattern: ({request}) => request.destination !== 'image',
+    //         handler: 'NetworkOnly',
+    //       },
+    //     ],
+    //   },
+    //   devOptions: {
+    //     enabled: true
+    //   }, 
+    //   manifest: {
+    //     name: 'Palworld Pal Editor',
+    //     short_name: 'Pal Editor',
+    //     description: 'Palworld Pal Editor, Authored by _connlost.',
+    //     theme_color: '#ffffff',
+    //     icons: [
+    //       {
+    //         src: '/icons/512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       }
+    //     ]
+    //   }
+    // })
   ],
   resolve: {
     alias: {
