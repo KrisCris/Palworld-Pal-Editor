e066ce5664832c98231130927329839e152ab2ac
connlost <4348524953.h@gmail.com>
2024-04-22 22:33:10 +0800

    slightly update docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,7 +17,7 @@ chown -R pn:pn /app
 
 
 
-cmd="gosu pn ./setup_and_run.sh"
+cmd="./setup_and_run.sh"
 
 # Check each environment variable and append it to the command if it exists
 if [ -n "$APP_LANG" ]; then cmd="$cmd --lang=\"$APP_LANG\""; fi
@@ -25,8 +25,8 @@ if [ -n "$APP_PORT" ]; then cmd="$cmd --port=$APP_PORT"; fi
 if [ -n "$MODE" ]; then cmd="$cmd --mode=\"$MODE\""; fi
 if [ -n "$SAVE_PATH" ]; then cmd="$cmd --path=\"$SAVE_PATH\""; fi
 if [ -n "$PASSWORD" ]; then cmd="$cmd --password=\"$PASSWORD\""; fi
-if [ -n "$INTERACTIVE" ]; then cmd="$cmd --interactive=\"$INTERACTIVE\""; fi
+if [ -n "$INTERACTIVE" ]; then cmd="$cmd --interactive"; fi
 
 echo "Launching: $cmd"
 
-eval exec $cmd
+eval exec "su pn -c '$cmd'"
