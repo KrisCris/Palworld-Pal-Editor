570b4542ba772c17c4a2e166faf307ce0923a8d0
connlost <4348524953.h@gmail.com>
2025-02-11 12:26:17 +0800

    fix issue related to empty player nickname

--- src/palworld_pal_editor/api/player.py
+++ src/palworld_pal_editor/api/player.py
@@ -105,7 +105,7 @@ def get_player_data():
 def player_to_dict(player: PlayerEntity):
     return {
         "InstanceId": str(player.PlayerUId),
-        "NickName": player.NickName,
+        "NickName": player.NickName or "",
         "Level": player.Level or 1,
         "HasViewingCage": player.has_viewing_cage(),
         "OtomoCharacterContainerId": str(player.OtomoCharacterContainerId),
